deab058c7fcc8b39bb265e4beecda38d12165dbf
BilgeRat178 <brentweppler@gmail.com>
2025-05-29 09:44:10 -0600

    rebuild initial commit

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  //base: process.env.VITE_BASE_PATH || '/',  // Use the VITE_BASE_PATH environment variable
-  plugins: [react()],
-});
